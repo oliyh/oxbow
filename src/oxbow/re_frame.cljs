fc95d47f05a608a51319ec19a787e413f4e752d9
Oliver Hine <git@oliy.co.uk>
2020-10-07 12:37:32 +0100

    use url if id not provided

--- src/oxbow/re_frame.cljs
+++ src/oxbow/re_frame.cljs
@@ -9,9 +9,9 @@
 
 (rf/reg-event-fx
  ::abort
- (fn [{:keys [db]} [_ id]]
-   (when-let [{:keys [abort]} (get-in db [::oxbow :sse-client id])]
-     (merge {:db (update-in db [::oxbow :sse-client] dissoc id)}
+ (fn [{:keys [db]} [_ id-or-url]]
+   (when-let [{:keys [abort]} (get-in db [::oxbow :sse-client id-or-url])]
+     (merge {:db (update-in db [::oxbow :sse-client] dissoc id-or-url)}
             (when abort
               {::abort abort})))))
 
@@ -21,14 +21,13 @@
 
 (rf/reg-event-fx
  ::sse-client
- (fn [{:keys [db]} [_ {:keys [id]
-                       :or {id (random-uuid)}
-                       :as opts}]]
+ (fn [{:keys [db]} [_ {:keys [id url] :as opts}]]
    (let [opts (reduce (fn [opts event-key]
                         (if (contains? opts event-key)
                           (update opts event-key dispatch-callback)
                           opts))
                       opts
                       [:on-event :on-close :on-error])
-         sse-client (core/sse-client opts)]
+         sse-client (core/sse-client opts)
+         id (or id url)]
      {:db (assoc-in db [::oxbow :sse-client id] sse-client)})))
