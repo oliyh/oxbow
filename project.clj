ee010b1bab8e51e8a6af742ad0ec29cdcb77fcc5
Oliver Hine <git@oliy.co.uk>
2023-10-26 21:26:53 +0100

    Version 0.1.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject oliyh/oxbow "0.1.5"
+(defproject oliyh/oxbow "0.1.6-SNAPSHOT"
   :description "A Server Sent Events (SSE) client for cljs based on js/fetch"
   :url "https://github.com/oliyh/oxbow"
   :license {:name "Eclipse Public License"
